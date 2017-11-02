4febaee3c9b920ff9cd950834670912c6499a197
Jimmy Rengga <rengga.jimmy@gmail.com>
2017-11-02 13:32:12 +0700

    initialize project

---
+++ src/main/java/io/github/jimmyrengga/explorespring/ExploreSpringApplication.java
@@ -0,0 +1,12 @@
+package io.github.jimmyrengga.explorespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreSpringApplication.class, args);
+	}
+}
